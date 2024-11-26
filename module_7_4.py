24e0fd86978603e8ee06cc3247e8e7cf12938b67
LeIvano <139815453+LeIvano@users.noreply.github.com>
2024-11-27 01:52:39 +0300

    done

---
+++ module_7_4.py
@@ -0,0 +1,17 @@
+team1_num = 5
+print("В команде Мастера кода участников: %s !" % team1_num)
+team2_num = 10
+print("Итого сегодня в командах участников: %s и %s !" % (team1_num, team2_num))
+
+
+score_2 = 20
+print("Команда Волшебники данных решила задач: {0} !".format(score_2))
+team1_time = 1234.324
+print(" Волшебники данных решили задачи за {} с !".format(team1_time))
+
+
+score_1, score_2 = 40, 42
+print(f'Команды решили {score_1} и {score_2} задач.')
+challenge_result = 'победа команды Мастера кода'
+print(f'Результат битвы: {challenge_result}!')
+print(f'Сегодня было решено {score_1 + score_2} задач, в среднем по {team1_time/score_1} секунды на задачу!.')
